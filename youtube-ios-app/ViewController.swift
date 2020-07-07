b69ea831d9083f95ead0d00ca6f43226b18af607
arhurdibon <arthur@themobilelife.com>
2020-07-07 07:19:18 +0200

    initial xcode project creation

---
+++ youtube-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-ios-app
+//
+//  Created by Arhur Dibon on 07/07/2020.
+//  Copyright © 2020 Arthur D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
